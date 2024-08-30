0d68e7a4301174133def17348b160e60aba04043
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-08-31 02:04:32 +0530

    added routing logic

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+    const navigate = useNavigate();
+
+    const handleClickButton = () => {
+        navigate("/app");
+    };
+    return (
+        <>
+            <div>Home</div>
+            <button
+                className="mt-4 p-2 bg-blue-600 text-white rounded  hover:bg-blue-100 hover:text-black"
+                onClick={handleClickButton}
+            >
+                Student
+            </button>{" "}
+            <br />
+            <button className="mt-4 p-2 bg-blue-600 text-white rounded  hover:bg-blue-100 hover:text-black">
+                Parent
+            </button>{" "}
+            <br />
+            <button className="mt-4 p-2 bg-blue-600 text-white rounded  hover:bg-blue-100 hover:text-black">
+                UNI
+            </button>
+        </>
+    );
+}
+
+export default Home;
