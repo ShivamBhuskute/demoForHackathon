1c916d680db340c5792b89f69c3f5f935c7661ae
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-09-26 21:07:11 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/': {  // Matches all requests starting with a forward slash (/)
+        target: 'https://backend-for-hackathon.vercel.app',
+        changeOrigin: true, // Adjust if CORS handling is different on backend
+      },
+    },
+  },
 })
